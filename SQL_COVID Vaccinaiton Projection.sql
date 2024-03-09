7c899d251cd5429dd684f5b1728536d59cfc5035
mikamclaren <mikamclaren09@gmail.com>
2024-03-09 15:24:25 -0600

    removed unnecessary comments

--- SQL_COVID Vaccinaiton Projection.sql
+++ SQL_COVID Vaccinaiton Projection.sql
@@ -16,6 +16,7 @@ order by 1,2
 
 --Looking at the total cases vs total deaths
 --Shows the likelihoood of dying if you contract Covid in your country
+
 select location, date, total_cases, total_deaths, (CONVERT(float, total_deaths) / NULLIF(CONVERT(float, total_cases), 0))*100 as DeathPercentage
 from Projects..CovidDeaths
 where continent is not null
@@ -25,6 +26,7 @@ order by 1,2
 
 --Looking at the total cases vs the population
 --Shows what percentage of population got Covid
+
 select location, date, population, total_cases, (CONVERT(float, total_cases) / NULLIF(CONVERT(float, population), 0))*100 as PercentPopulationInfected
 from Projects..CovidDeaths
 where continent is not null
@@ -33,6 +35,7 @@ order by 1,2
 
 
 --countries with the highest infection rate compared to population
+
 select location, population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population))*100 as PercentPopulationInfected
 from Projects..CovidDeaths
 --where location like '%states%'
@@ -51,7 +54,7 @@ group by location
 order by TotalDeathCount desc
 
 
---LET'S BREAK THINGS DOWN BY CONTINENT
+--BREAK THINGS DOWN BY CONTINENT
 
 select location, MAX(cast(total_deaths as int)) as TotalDeathCount
 from Projects..CovidDeaths
@@ -61,8 +64,9 @@ group by location
 order by TotalDeathCount desc
 
 
---Showing the continents with the highest death cont per population
+--Showing the continents with the highest death count per population
 --This will  group by continent, but the numbers will not be as accurate as in the above
+
 select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
 from Projects..CovidDeaths
 --where location like '%states%'
@@ -83,6 +87,7 @@ order by 1,2
 
 
 --Looking at total population vs vaccinations
+
 Select *
 from Projects..CovidDeaths dea
 join Projects..CovidVaccinations vac
@@ -90,7 +95,6 @@ join Projects..CovidVaccinations vac
 	and dea.date = vac.date
 
 
---this casts an error due to the third line below because you cannot use a newly created column to do calculations on. See 'Create CTE' below
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location order by dea.location, 
 dea.date) as RollingPeopleVaccinated
@@ -103,7 +107,8 @@ order by 2, 3
 
 
 
---Create CTE (order by clause cannot be in the CTE, so it has been commented out)
+--Create CTE 
+
 With PopvsVac (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -115,7 +120,6 @@ join Projects..CovidVaccinations vac
 	on dea.location = vac.location 
 	and dea.date = vac.date
 where dea.continent is not null
---order by 2, 3
 )
 
 Select *, (RollingPeopleVaccinated/population)*100
@@ -123,11 +127,9 @@ from PopvsVac
 
 
 
-
-
 --Temp Table
 
-Drop table if exists #PercentPopulationVaccinated -- add drop table if exists so you can continually make updates to the table without creating an already exists error
+Drop table if exists #PercentPopulationVaccinated 
 Create table #PercentPopulationVaccinated
 (
 Continent nvarchar(255),
@@ -154,7 +156,7 @@ from #PercentPopulationVaccinated
 
 
 
--- Creating View to store data for later visualizations (cannot use order by clause in views either)
+-- Creating View to store data for later visualizations 
 
 Create view PercentPopulationVaccinated as
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
